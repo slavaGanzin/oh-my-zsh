8d2b5af388eae79bba01aa3bf07074e7223731ef
slavaGanzin <slava.ganzin@gmail.com>
2016-10-11 11:57:45 +0300

    git pullr - git alias for pull with rebase git pullrs - same with autostash

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -238,3 +238,6 @@ alias glum='git pull upstream master'
 
 alias gwch='git whatchanged -p --abbrev-commit --pretty=medium'
 alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify -m "--wip--"'
+
+git config --global alias.pullrs "pull --rebase --autostash"
+git config --global alias.pullr "pull --rebase"
